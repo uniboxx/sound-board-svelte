380ba1899caa1678996db98056a1cd469f349b03
uniboxx <unibox@duck.com>
2024-08-17 00:32:40 +0200

    finish project

--- src/App.svelte
+++ src/App.svelte
@@ -5,16 +5,12 @@
   import { audioState } from './state/globalState.svelte';
 
   const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
-  const base = '/sound-board-svelte';
 
-  let audio = $state();
-  let isPaused = $state(true);
-  let currentTime = $state(0);
-  let btnaudio = $state();
+  let isPlaying = $state('');
 </script>
 
 <div id="buttons">
   {#each sounds as sound (sound)}
-    <BtnAudio bind:this={btnaudio} soundName={sound} />
+    <BtnAudio bind:isPlaying soundName={sound} />
   {/each}
 </div>
--- src/lib/BtnAudio.svelte
+++ src/lib/BtnAudio.svelte
@@ -1,30 +1,45 @@
 <script>
-  // @ts-nocheck
   import { onMount } from 'svelte';
   import { audioState } from '../state/globalState.svelte';
 
-  let { soundName } = $props();
+  let audio;
+  let { soundName, isPlaying = $bindable() } = $props();
 
-  const base = '/sound-board-svelte';
-  onMount(() => {
-    if (!audioState.audios.some(el => el.id === audio.id))
-      audioState.audios.push(audio);
-  });
-
-  function handleClick() {
-    audioState.audios.forEach(audio => {
+  $effect(() => {
+    if (soundName !== isPlaying) {
       audio.pause();
       audio.timeupdate = 0;
-      audio.classList.add('ciao');
-      console.log(audioState.audios);
-    });
+    }
+  });
+
+  const base = '/sound-board-svelte';
+
+  function handleClick() {
+    isPlaying = soundName;
     audio.play();
   }
-
-  let audio;
 </script>
 
 <audio bind:this={audio} id={soundName} src={`${base}/sounds/${soundName}.mp3`}
 ></audio>
 
 <button class="btn" onclick={handleClick}>{soundName}</button>
+
+<style lang="scss">
+  .btn {
+    background-color: rebeccapurple;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    margin: 1rem;
+    padding: 1.5rem 3rem;
+    font-size: 1.2rem;
+    font-family: inherit;
+  }
+  .btn:hover {
+    opacity: 0.9;
+  }
+  .btn:focus {
+    outline: none;
+  }
+</style>
